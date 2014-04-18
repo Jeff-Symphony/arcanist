19a1350a438640b99fab9ea6421639111327bbab
Jeff-Symphony <jknurek@symphonycommerce.com>
2014-04-18 08:52:28 -0700

    create a class to extend ArcanistLintEngine specific for our goals

---
+++ src/lint/engine/SymphonyLintEngine.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This lint engine applies the @{class:ArcanistPyLintLinter} to Python 
+ * files, and @{class:ArcanistJSHintLinter} to JS files. 
+ *
+ * @group linter
+ */
+final class SymphonyLintEngine extends ArcanistLintEngine {
+
+  public function buildLinters() {
+
+    // This is a list of paths to lint. 
+    // Either provided them explicitly with `--lintall`,
+    // or arc figures them out from a set of changes. 
+    $paths = $this->getPaths();
+
+    // Remove any paths that don't exist before we add paths to linters.
+    foreach ($paths as $key => $path) {
+      if (!$this->pathExists($path)) {
+        unset($paths[$key]);
+      }
+    }
+
+    /*
+    foreach ($paths as $path) {
+      if (!preg_match('/\.py$/', $path)) {
+        continue;
+      }
+      // an example of how to exclude a path?
+      if (preg_match('@^externals/@', $path)) {
+        continue;
+      }
+    }
+    */
+
+    $py_paths = preg_grep('/\.py$/', $paths);
+    $linters[] = id(new ArcanistPyLintLinter())->setPaths($py_paths);
+
+    $js_paths = preg_grep('/\.js$/', $paths);
+    $linters[] = id(new ArcanistJSHintLinter())->setPaths($js_paths);
+
+    return $linters;
+  }
+
+}
